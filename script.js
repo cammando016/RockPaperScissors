c8b5c2ead268239f01897bf3a705accb565ff3dd
Cameron Anderson <cammando@hotmail.com.au>
2024-06-23 21:16:48 +1000

    add UI to play game by clicking Rock, Paper or Scissors button instead of typing in selection

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 const gameOptions = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let compScore = 0;
+let gameOver = false;
+
+const playerScoreLabel = document.querySelector("#player-score");
+const compScoreLabel = document.querySelector("#comp-score");
+
+playerScoreLabel.textContent = playerScore;
+compScoreLabel.textContent = compScore;
 
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random()*gameOptions.length);
     return gameOptions[computerChoice];
 }
 
+let playerChoice = document.querySelector("#playerChoice");
+playerChoice.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch(target.id) {
+        case "rock":
+            playerSelection = target.id;
+            break;
+        case "paper":
+            playerSelection = target.id;
+            break;
+        case "scissors":
+            playerSelection = target.id;
+            break;
+    }
+
+    console.log(playerSelection);
+
+    playRound(playerSelection, getComputerChoice());
+})
+
 function playRound(playerSelection, computerSelection){
     let result;
     let winner;
+    gameOver = false;
 
     console.log(`Player Choice: ${playerSelection}`);
     console.log(`Computer Choice: ${computerSelection}`);
@@ -50,41 +79,46 @@ function playRound(playerSelection, computerSelection){
     console.log(result);
     if (result.charAt(4) === "l"){
         winner = "Computer";
+
     }
     else if (result.charAt(4) === "w"){
         winner = "Player";
     }
     else {winner = "Tie";}
-    return winner;
-}
 
-//console.log(playRound(prompt("Enter your choice"), getComputerChoice()));
+    updateScore(winner);
+    checkWinner(playerScore, compScore);
 
-function playGame(){
-    for (i = 0; i < 5; i++){
-        let roundWinner = playRound(prompt("Enter your choice"), getComputerChoice());
-        if(roundWinner == "Computer"){
-            compScore++;
-        }
-        else if (roundWinner == "Player") {
-            playerScore++;
-        }
-
-        console.log(`Score\nPlayer: ${playerScore}\nComputer: ${compScore}`);
+    if(gameOver){
+        resetScores();
     }
 }
 
-function checkWinner(compScore, playerScore){
-    if (compScore > playerScore){
-        return "Final Winner: Computer";
+function checkWinner(playerScore, compScore) {
+    if (playerScore === 5){
+        alert("You Win!");
+        gameOver = true;
     }
-    else if(compScore < playerScore){
-        return "Final Winner: You!";
-    }
-    else{
-        return "Final Result: It's a tie";
+    if (compScore === 5){
+        alert("You lose :(");
+        gameOver = true;
     }
 }
 
-playGame();
-console.log(checkWinner(compScore, playerScore));
+function resetScores() {
+    playerScore = 0;
+    compScore = 0;
+    playerScoreLabel.textContent = "0";
+    compScoreLabel.textContent = "0";
+}
+
+function updateScore(winner) {
+    if (winner === "Player") {
+        playerScore++;
+        playerScoreLabel.textContent = playerScore;
+    }
+    else if (winner === "Computer") {
+        compScore++;
+        compScoreLabel.textContent = compScore;
+    }
+}
